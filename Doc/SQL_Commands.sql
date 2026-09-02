0621f2d0986e6789f717c7bbea093cb2cf5ade65
Ndondeni <st10153048@rcconnect.edu.za>
2026-09-02 17:19:02 +0200

    Update SQL_Commands.sql
    Creating tables

--- Doc/SQL_Commands.sql
+++ Doc/SQL_Commands.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE MarathonManagementSystem;
 
 USE MarathonManagementSystem;
+
+--Creating Tables
+--Role
+CREATE TABLE Roles (
+    RoleID INT AUTO_INCREMENT PRIMARY KEY,
+    RoleName VARCHAR(50) NOT NULL UNIQUE
+);
