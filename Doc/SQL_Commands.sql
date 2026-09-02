4a5abc803e85aae7493b1908a5faa59e9d67e761
Ndondeni <st10153048@rcconnect.edu.za>
2026-09-02 17:24:02 +0200

    Adding User table & Instering Roles

--- Doc/SQL_Commands.sql
+++ Doc/SQL_Commands.sql
@@ -8,3 +8,22 @@ CREATE TABLE Roles (
     RoleID INT AUTO_INCREMENT PRIMARY KEY,
     RoleName VARCHAR(50) NOT NULL UNIQUE
 );
+INSERT INTO Roles (RoleName)
+VALUES
+    ('Administrator'),
+    ('Organizer'),
+    ('Participant');
+--User 
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    RoleID INT NOT NULL,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    Email VARCHAR(150) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20),
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (RoleID)
+        REFERENCES Roles(RoleID)
+);
